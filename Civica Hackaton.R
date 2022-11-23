9fd6c2a0052661330f2e07a7127f300064ef2f59
Lucho Barajas <114389530+LuchoBarajas@users.noreply.github.com>
2022-11-23 17:19:41 +0000

    R code Modified

--- Civica Hackaton.R
+++ Civica Hackaton.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(maps)
 library(plotly)
 library(openxlsx)
+library(geobr)
 
 setwd("/Users/luchobarajas/Documents/ASDS/CIVICA Hackaton/")
 
@@ -12,40 +13,49 @@ wildfiresBz = read.csv("modis_2021_Brazil.csv", sep = ";")
 
 map.data <- map_data("world")
 
-# To check max and min of longitude and latitude for setting scale below
-# max(tweets$longitude)
-# min(tweets$longitude)
-# max(tweets$latitude)
-# min(tweets$latitude)
 
-# Now map using ggplot
-# Set up map background using map.data data frame
-B1 = ggplot(map.data) + geom_map(aes(map_id = region), map = map.data, fill = "black", 
-                            color = "white", size = 0.25) + expand_limits(x = map.data$long, y = map.data$lat) + 
-  # Limits for x and y axis based on min and max of longitude and latitude
-  scale_x_continuous(limits=c(-75, -33)) + scale_y_continuous(limits = c(-37, 8)) +
-  # Adding the dot for each tweet and specifying dot size, transparency, and colour by language
-  geom_point(data = wildfiresBz, aes(x = longitude, y = latitude, color = "red"), show.legend = FALSE, size = 0.01,
-             alpha = 0.01) +
-  # Removing unnecessary graph elements + changing background to black to match inspiratin from Pablo Barberá's twitter page
+br = read_state()
+deforest = read.xlsx("BRA.xlsx", sheet = 4)
+deforest_2021 = deforest %>% select(name_state = subnational1, area = extent_2010_ha, threshold, 
+                                    deforestation_2021 = tc_loss_ha_2021) %>% 
+  filter(threshold == 10) %>% select(-threshold) %>% mutate(Def_ratio =deforestation_2021 / area)
+
+deforest_2021 %<>% mutate(name_state = if_else(name_state == "Rio Grande do Norte","Rio Grande Do Norte", name_state))
+deforest_2021 %<>% mutate(name_state = if_else(name_state == "Rio Grande do Sul","Rio Grande Do Sul", name_state))
+deforest_2021 %<>% mutate(name_state = if_else(name_state == "Espírito Santo","Espirito Santo", name_state))
+deforest_2021 %<>% mutate(name_state = if_else(name_state == "Rio de Janeiro","Rio De Janeiro", name_state))
+deforest_2021 %<>% mutate(name_state = if_else(name_state == "Mato Grosso do Sul","Mato Grosso Do Sul", name_state))
+
+
+
+br %<>% left_join(deforest_2021)
+
+ggplot()+
+  geom_sf(data = br, aes(fill= Def_ratio), color= "white", size=.15, show.legend = FALSE) +
+  scale_fill_gradient( low = "green" , high = "black")+
+  geom_point(data = wildfiresBz, aes(x = longitude, y = latitude, color = "firebrick1"), show.legend = FALSE, size = 0.01,
+          alpha = 0.10) + 
   theme(axis.line = element_blank(), 
         axis.text = element_blank(), 
         axis.ticks = element_blank(), 
         axis.title = element_blank(), 
-        panel.background = element_rect(fill = "black", color = NA), 
+        panel.background = element_rect(fill = "white", color = NA), 
         panel.border = element_blank(), 
         panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank(), 
-        plot.background = element_rect(fill= "black")) +
-  # Add title directly onto graph via annotate
-  #annotate("text", x=-1, y=68, label="Tweets by Language", size=6, color = "white", fontface = 2)
-  annotate("text", x=-1, y=68, label='underline("Tweets by Language")', parse = TRUE, size=6, color = "white")
+        panel.grid.minor = element_blank())
+ 
 
-B1
 
-install.packages("geobr")
-library(geobr)
-
-br = read_state()
-deforest = read.xlsx("BRA.xlsx", sheet = 4)
-deforest_2021 = deforest %>% select(name_state = subnational1, deforestation_2021 = tc_loss_ha_2021)
+ggplot()+
+  geom_sf(data = br, aes(fill= Def_ratio), color= "white", size=.15, show.legend = FALSE) +
+ scale_fill_distiller(palette = "Spectral", name="Deforestation", limits = c(55,707681)) +
+  geom_point(data = wildfiresBz, aes(x = longitude, y = latitude, color = "firebrick1"), show.legend = FALSE, size = 0.01,
+             alpha = 0.10) + 
+  theme(axis.line = element_blank(), 
+        axis.text = element_blank(), 
+        axis.ticks = element_blank(), 
+        axis.title = element_blank(), 
+        panel.background = element_rect(fill = "white", color = NA), 
+        panel.border = element_blank(), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank())
